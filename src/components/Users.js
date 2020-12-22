1ee764bb13ef19fc9ca74c30675f25992155a244
enginecode-cpu <bassmatics1002@gmail.com>
2020-12-22 14:57:38 +0900

    useReducer를 활용

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,29 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
+// Loading, Success, Error
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        erorr: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unabled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
-      setUsers(response.data);
+      dispatch({ type: "SUCCESS", data: response.data });
     } catch (e) {
-      setError(e);
+      console.log(e);
+      dispatch({ type: "ERROR", error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>Loading...</div>;
 
